96b2cdd5e5963b1877c596fa41d362e8f4e77b47
Ian McNally <ianmcnally@users.noreply.github.com>
2017-01-09 13:14:10 -0500

    add acceptance tests for app

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import App from './App';
 import renderShallow from 'render-shallow'
+import { findWithType } from 'react-shallow-testutils'
+import { Instructions, RemainingBalance, MaximumSpend } from './components'
 
 describe('when it renders', () => {
   let component
@@ -11,6 +13,72 @@ describe('when it renders', () => {
 
   it('renders the App components', () => {
     expect(component).toMatchSnapshot()
+  })
+
+})
+
+describe('when the remaining balance changes', () => {
+  const mockChangeEvent = {}
+  const mockChangeValue = '1.00'
+  let remainingBalanceComponent
+
+  beforeAll(() => {
+    const { output, rerender } = renderShallow(<App />)
+
+    const remainingBalance = findWithType(output, RemainingBalance)
+    remainingBalance.props.onChange(mockChangeEvent, mockChangeValue)
+
+    const nextOutput = rerender()
+    remainingBalanceComponent = findWithType(nextOutput, RemainingBalance)
+  })
+
+  it('updates the value passed to <RemainingBalance>', () => {
+    expect(remainingBalanceComponent.props.value).toEqual(mockChangeValue)
+  })
+
+})
+
+describe('when the maximum spend changes', () => {
+  const mockChangeEvent = {}
+  const mockChangeValue = '1.00'
+  let maximumSpendComponent
+
+  beforeAll(() => {
+    const { output, rerender } = renderShallow(<App />)
+
+    const maximumSpend = findWithType(output, MaximumSpend)
+    maximumSpend.props.onChange(mockChangeEvent, mockChangeValue)
+
+    const nextOutput = rerender()
+    maximumSpendComponent = findWithType(nextOutput, MaximumSpend)
+  })
+
+  it('updates the value passed to <MaximumSpend>', () => {
+    expect(maximumSpendComponent.props.value).toEqual(mockChangeValue)
+  })
+
+})
+
+describe('when there are a remaining balance and maximum spend values', () => {
+  const mockChangeEvent = {}
+  const mockChangeValue = '1.00'
+  let instructionsComponent
+
+  beforeAll(() => {
+    const { output, rerender } = renderShallow(<App />)
+
+    const maximumSpend = findWithType(output, MaximumSpend)
+    maximumSpend.props.onChange(mockChangeEvent, mockChangeValue)
+
+    const remainingBalance = findWithType(output, RemainingBalance)
+    remainingBalance.props.onChange(mockChangeEvent, mockChangeValue)
+
+    const nextOutput = rerender()
+    instructionsComponent = findWithType(nextOutput, Instructions)
+  })
+
+  it('sets hide=true on <Instructions>', () => {
+    expect(instructionsComponent.props.hide).toBe(true)
   })
 
 })
