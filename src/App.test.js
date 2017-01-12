1c5b1bc1c0cf780f73ac06a41e2ca3bb809fcf37
Ian McNally <ianmcnally@users.noreply.github.com>
2017-01-11 20:15:27 -0500

    add test for cost calculation

--- src/App.test.js
+++ src/App.test.js
@@ -2,7 +2,8 @@ import React from 'react';
 import App from './App';
 import renderShallow from 'render-shallow'
 import { findWithType } from 'react-shallow-testutils'
-import { Instructions, RemainingBalance, MaximumSpend } from './components'
+import { Additions, Instructions, RemainingBalance, MaximumSpend } from './components'
+import { costForFaresInBudget } from './services/fare-calculations'
 
 describe('when it renders', () => {
   let component
@@ -61,24 +62,31 @@ describe('when the maximum spend changes', () => {
 
 describe('when there are a remaining balance and maximum spend values', () => {
   const mockChangeEvent = {}
-  const mockChangeValue = '1.00'
+  const mockBalanceValue = '1.00'
+  const mockSpendValue = '40.00'
   let instructionsComponent
+  let additionsComponent
 
   beforeAll(() => {
     const { output, rerender } = renderShallow(<App />)
 
     const maximumSpend = findWithType(output, MaximumSpend)
-    maximumSpend.props.onChange(mockChangeEvent, mockChangeValue)
+    maximumSpend.props.onChange(mockChangeEvent, mockSpendValue)
 
     const remainingBalance = findWithType(output, RemainingBalance)
-    remainingBalance.props.onChange(mockChangeEvent, mockChangeValue)
+    remainingBalance.props.onChange(mockChangeEvent, mockBalanceValue)
 
     const nextOutput = rerender()
     instructionsComponent = findWithType(nextOutput, Instructions)
+    additionsComponent = findWithType(nextOutput, Additions)
   })
 
   it('sets hide=true on <Instructions>', () => {
     expect(instructionsComponent.props.hide).toBe(true)
+  })
+
+  it('passes the fare calculations to <Additions>', () => {
+    expect(additionsComponent.props.fares).toEqual(costForFaresInBudget(mockBalanceValue, mockSpendValue))
   })
 
 })
