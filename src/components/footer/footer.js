8f54869985097d8a9d65985c31607453fbcab42f
Ian McNally <imcnally@2u.com>
2018-10-27 17:09:35 -0600

    Add memoization to components that can be optimized

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -1,9 +1,10 @@
 import React from 'react'
 
-const Footer = () =>
+const Footer = React.memo(() =>
   <footer className="f6 bt b--black-20">
     <p>Say hello <a href="twitter.com/farelyapp">@farelyapp</a></p>
   </footer>
+)
 
 export default Footer
 
--- src/components/instructions/instructions.js
+++ src/components/instructions/instructions.js
@@ -3,22 +3,23 @@ import PropTypes from 'prop-types'
 
 const getClassNamesForInstructions = shouldHideInstructions => {
   let classNames = 'f5 lh-copy measure-narrow'
-  if (shouldHideInstructions)
-    classNames += ' dn'
+  if (shouldHideInstructions) classNames += ' dn'
   return classNames
 }
 
-const Instructions = ({ hide }) =>
+const Instructions = React.memo(({ hide }) => (
   <p className={getClassNamesForInstructions(hide)}>
-    Enter your MetroCard’s current balance to calculate the amount to put on it for an exact number of rides.
+    Enter your MetroCard’s current balance to calculate the amount to put on it
+    for an exact number of rides.
   </p>
+))
 
 Instructions.propTypes = {
-  hide: PropTypes.bool.isRequired
+  hide: PropTypes.bool.isRequired,
 }
 
 Instructions.defaultProps = {
-  hide: false
+  hide: false,
 }
 
 export default Instructions
