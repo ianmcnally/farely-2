6a91096b13d744fa180d2ad54d4a52806495fc1d
Ian McNally <imcnally@2u.com>
2019-04-28 14:24:54 -0400

    Move styles into separate import

--- src/App.js
+++ src/App.js
@@ -7,32 +7,12 @@ import {
   Footer,
 } from './components'
 import { costForFaresInBudget } from './services/fare-calculations'
-import 'normalize.css'
-import 'tachyons-borders/css/tachyons-borders.min.css'
-import 'tachyons-border-colors/css/tachyons-border-colors.min.css'
-import 'tachyons-display/css/tachyons-display.min.css'
-import 'tachyons-font-family/css/tachyons-font-family.min.css'
-import 'tachyons-font-weight/css/tachyons-font-weight.min.css'
-import 'tachyons-forms/css/tachyons-forms.min.css'
-import 'tachyons-line-height/css/tachyons-line-height.min.css'
-import 'tachyons-max-widths/css/tachyons-max-widths.min.css'
-import 'tachyons-spacing/css/tachyons-spacing.min.css'
-import 'tachyons-type-scale/css/tachyons-type-scale.min.css'
-import 'tachyons-typography/css/tachyons-typography.min.css'
-import 'tachyons-utilities/css/tachyons-utilities.min.css'
-import 'tachyons-widths/css/tachyons-widths.min.css'
-import 'tachyons-lists/css/tachyons-lists.min.css'
-import 'tachyons-border-style/css/tachyons-border-style.min.css'
-import 'tachyons-border-widths/css/tachyons-border-widths.min.css'
+import './styles'
 
 const defaultCurrentBalance = '0.00'
 const defaultMaximumSpend = '40.00'
-
 const initialState = {
-  fares: costForFaresInBudget(
-    defaultCurrentBalance,
-    defaultMaximumSpend,
-  ),
+  fares: costForFaresInBudget(defaultCurrentBalance, defaultMaximumSpend),
   currentBalance: defaultCurrentBalance,
   maximum: defaultMaximumSpend,
 }
@@ -70,10 +50,7 @@ export default function App() {
 
   return (
     <main className="sans-serif mw5 center">
-      <RemainingBalance
-        value={currentBalance}
-        onChange={handleBalanceChange}
-      />
+      <RemainingBalance value={currentBalance} onChange={handleBalanceChange} />
       <MaximumSpend value={maximum} onChange={handleMaximumChange} />
       <Additions fares={fares} />
       <Instructions hide={hideInstructions} />
