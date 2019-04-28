a4dde67a45c78849e735188faf73b26a96ed2f73
Ian McNally <imcnally@2u.com>
2019-04-28 11:47:11 -0400

    Updates

---
+++ precache-manifest.4608c0dc56626f5e47b5369ad2a26d93.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eedc857eb62eaed15d103163e508aab9",
+    "url": "/farely-2/index.html"
+  },
+  {
+    "revision": "cf7fed5bb538b751a7cc",
+    "url": "/farely-2/static/css/2.c60bfa77.chunk.css"
+  },
+  {
+    "revision": "cf7fed5bb538b751a7cc",
+    "url": "/farely-2/static/js/2.c07a41ce.chunk.js"
+  },
+  {
+    "revision": "74fc092b82963d53eaf9",
+    "url": "/farely-2/static/js/main.7194adc3.chunk.js"
+  },
+  {
+    "revision": "bad1accd3b8edb0e0115",
+    "url": "/farely-2/static/js/runtime~main.b2dc5973.js"
+  }
+]);
