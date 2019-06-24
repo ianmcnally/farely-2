8360a648f4589e400ebbd005f4056fe50302ab05
Ian McNally <ianmcnally@users.noreply.github.com>
2019-06-24 10:39:11 -0400

    Refactor fare calculation to avoid repetition

--- src/services/fare-calculations.js
+++ src/services/fare-calculations.js
@@ -1,32 +1,37 @@
-import {
-  TRANSACTION_MAX,
-  RIDE_COST
-} from '../constants'
-import range from 'lodash.range'
+import { TRANSACTION_MAX, RIDE_COST } from '../constants'
 
-const getPossibleFaresForSpendingRange = (currentBalance, maximumSpend) => {
-  const ridesLeftOnCard = (currentBalance > RIDE_COST) ? Math.floor(currentBalance / RIDE_COST) : 0
+const getFaresForSpendingRange = (currentBalance, maximumSpend) => {
+  const ridesLeftOnCard =
+    currentBalance > RIDE_COST ? Math.floor(currentBalance / RIDE_COST) : 0
   const amountOfRidesPossibleWithinBudget = Math.floor(maximumSpend / RIDE_COST)
+  const fares = []
 
-  return range(ridesLeftOnCard + 1, amountOfRidesPossibleWithinBudget + 1).map(rideCount => ({
-    rides: rideCount,
-    fare: rideCount * RIDE_COST
-  }))
+  for (
+    let rideCount = ridesLeftOnCard + 1;
+    rideCount <= amountOfRidesPossibleWithinBudget;
+    rideCount++
+  ) {
+    const fare = rideCount * RIDE_COST
+    const cost = fare - currentBalance
+    if (isValidAmountToAdd(cost, maximumSpend)) {
+      fares.push({ rides: rideCount, cost })
+    }
+  }
+
+  return fares
 }
 
-const getCostsForFares = (currentBalance, fares) => fares.map(({ rides, fare }) => {
-  const costOfFare = fare - currentBalance
-  return { rides, cost: costOfFare }
-})
+const isDollarAmountAMultipleOfFive = dollarAmount =>
+  (dollarAmount * 100) % 5 === 0
 
-const isDollarAmountAMultipleOfFive = dollarAmount => ((dollarAmount * 100) % 5) === 0
+const isValidAmountToAdd = (value, maximumValue) =>
+  isDollarAmountAMultipleOfFive(value) && value <= maximumValue
 
-const isValidAmountToAdd = (value, maximumValue) => isDollarAmountAMultipleOfFive(value) && (value <= maximumValue)
-
-export const costForFaresInBudget = (currentBalance = 0, maximumSpend = TRANSACTION_MAX) => {
-  const maximumAmountToSpend = (maximumSpend > TRANSACTION_MAX) ? TRANSACTION_MAX : maximumSpend
-  const possibleFares = getPossibleFaresForSpendingRange(currentBalance, maximumAmountToSpend)
-  const possibleCosts = getCostsForFares(currentBalance, possibleFares)
-  const validAmountsToAdd = possibleCosts.filter(({ cost }) => isValidAmountToAdd(cost, maximumAmountToSpend))
-  return validAmountsToAdd
+export const costForFaresInBudget = (
+  currentBalance = 0,
+  maximumSpend = TRANSACTION_MAX,
+) => {
+  const maximumAmountToSpend =
+    maximumSpend > TRANSACTION_MAX ? TRANSACTION_MAX : maximumSpend
+  return getFaresForSpendingRange(currentBalance, maximumAmountToSpend)
 }
